bc4c15e36558480608b53c96f149f124631372fb
yurisanford <yurisanford@fortestecnologia.com.br>
2023-09-10 12:18:10 -0300

    Criando repositório para pessoa

---
+++ EstudoAPI.Domain/Interfaces/Commun/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoAPI.Domain.Repositories
+{
+    public interface IRepository<TModel>
+    {
+
+        void Create(TModel model);
+        void Update(TModel model);
+        void Delete(TModel model);
+        IQueryable<TModel> GetAll();
+        TModel Get(int id);
+
+    }
+}
---
+++ EstudoAPI.Domain/Interfaces/Data/IPessoaRepository.cs
@@ -0,0 +1,9 @@
+﻿using EstudoAPI.Domain.Entities;
+using EstudoAPI.Domain.Repositories;
+
+namespace EstudoAPI.Domain.Interfaces.Data
+{
+    public interface IPessoaRepository : IRepository<Pessoa>
+    {
+    }
+}
